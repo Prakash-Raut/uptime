74441099d5d745e208118b4ff38cb15250b50ce9
Prakash-Raut <prakash_raut@outlook.com>
2025-11-21 12:54:00 +0530

    fix: correct import paths for PrismaClient in index.ts
    - Updated import statements for PrismaClient to use relative paths, ensuring compatibility and proper module resolution.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../prisma/generated/client.js";
+import { PrismaClient } from "./prisma/generated/client.js";
 
 type GetDbParams = {
 	connectionString: string;
@@ -17,7 +17,7 @@ function getDb({ connectionString }: GetDbParams): PrismaClient {
 // The inferred type of 'prisma' cannot be named without a reference to '../node_modules/@repo/database/src/generated/prisma/client'.
 // This is likely not portable. A type annotation is necessary.ts(2742)
 // ```
-export type { PrismaClient } from "../prisma/generated/client";
+export type { PrismaClient } from "./prisma/generated/client";
 
 const db = getDb({ connectionString: process.env.DATABASE_URL as string });
 
