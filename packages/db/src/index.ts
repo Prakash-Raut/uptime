c5a0a449e5ec024ea6acb08b6ff916f6eb600e1a
Prakash-Raut <prakash_raut@outlook.com>
2025-11-21 13:35:49 +0530

    refactor: streamline database connection setup in index.ts
    - Removed the getDb function and directly instantiated PrismaClient with the database connection pool, simplifying the code structure.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,24 +1,9 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/client.js";
 
-type GetDbParams = {
-	connectionString: string;
-};
-
-function getDb({ connectionString }: GetDbParams): PrismaClient {
-	const pool = new PrismaPg({ connectionString });
-	const prisma = new PrismaClient({ adapter: pool });
-
-	return prisma;
-}
-
-// This export is needed to avoid the TypeScript error:
-// ```
-// The inferred type of 'prisma' cannot be named without a reference to '../node_modules/@repo/database/src/generated/prisma/client'.
-// This is likely not portable. A type annotation is necessary.ts(2742)
-// ```
-export type { PrismaClient } from "../prisma/generated/client.js";
-
-const db = getDb({ connectionString: process.env.DATABASE_URL as string });
+const pool = new PrismaPg({
+	connectionString: process.env.DATABASE_URL as string,
+});
+const db = new PrismaClient({ adapter: pool });
 
 export default db;
