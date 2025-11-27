dad74029ab339502889804c502e3e7bb1fc5c24c
Prakash-Raut <prakash_raut@outlook.com>
2025-11-27 12:26:09 +0530

    fix: correct start script in package.json and update tsdown configuration for entry point
    - Modified the start script in package.json to use the correct command for running the server.
    - Updated tsdown.config.ts to change the entry point to include all TypeScript files in the src directory, improving build flexibility.

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -1,20 +1,11 @@
-import path from "node:path";
 import { defineConfig, type UserConfig } from "tsdown";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const config: UserConfig = defineConfig({
-	entry: "./src/index.ts",
+	entry: "./src/**/*",
 	format: "esm",
 	outDir: "./dist",
 	clean: true,
 	noExternal: [/@uptime\/.*/],
-
-	alias: {
-		"@": path.resolve(__dirname, "src"),
-	},
 });
 
 export default config;
