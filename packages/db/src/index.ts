2ea834020d1c1478b780bfcb3041717efa72d0e6
Prakash-Raut <prakash_raut@outlook.com>
2025-12-02 14:09:29 +0530

    chore: update package.json exports and simplify DB client initialization
    - Updated export paths in the auth, logger, and redis-client packages to reference the correct compiled output in dist.
    - Simplified the DB client initialization by removing unnecessary connection string handling in index.ts, directly using the environment variable for the Prisma client.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,24 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/client";
 
-type GetDbParams = {
-	connectionString: string;
-};
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-export function getDb({ connectionString }: GetDbParams): PrismaClient {
-	const pool = new PrismaPg({ connectionString });
-	const prisma = new PrismaClient({ adapter: pool });
+const prisma = new PrismaClient({ adapter });
 
-	return prisma;
-}
-
-export type { PrismaClient } from "../prisma/generated/client";
-
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-	throw new Error("DATABASE_URL environment variable is not set");
-}
-
-const db = getDb({ connectionString });
-export default db;
+export default prisma;
